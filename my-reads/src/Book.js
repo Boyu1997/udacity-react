0c2ddbbd64881f29111a88ead93429fd12999034
Boyu Jiang <jbyjiangboyu@126.com>
2018-05-22 10:39:10 +0800

    Compoment the page

--- my-reads/src/Book.js
+++ my-reads/src/Book.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import BookChanger from './BookChanger.js'
 
 class Book extends Component {
-  state = {
-    title: 'To Kill a Mockingbird',
-    author: 'Harper Lee'
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
   }
   render () {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.url }}></div>
           <BookChanger />
         </div>
-        <div className="book-title">{this.state.title}</div>
-        <div className="book-authors">{this.state.author}</div>
+        <div className="book-title">{this.props.title}</div>
+        <div className="book-authors">{this.props.author}</div>
       </div>
     )
   }
--- my-reads/src/ListBooks.js
+++ my-reads/src/ListBooks.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Book from './Book.js'
 
 class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    shelfTitle: PropTypes.string.isRequired
+  }
 
   render () {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                  
-                </div>
-                <div className="book-title">Ender's Game</div>
-                <div className="book-authors">Orson Scott Card</div>
-              </div>
-            </li>
+
+            {this.props.books.map((book) => (
+              <li key={book.title}>
+                <Book title={book.title}
+                  author={book.author}
+                  url={book.url}
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
